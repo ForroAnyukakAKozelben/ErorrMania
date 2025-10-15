3428d565ede686ddfe61e60d2bd90c2cdbce0d64
Hot Dominik <forro.dominik.adrian@diak.szbi-pg.hu>
2025-10-15 10:41:49 +0200

    My little Pony

---
+++ 2025.10.15_lists/2_feladat.py
@@ -0,0 +1,18 @@
+"""
+2. Feladat
+A program generáljon 10 darab véletlenszámot [1;3] intervallumon, ezeket tárolja egy listában, 
+a lista tartalmát írja ki a képernyőre! A felhasználónak legyen lehetősége megadni egy számot [1;3] intervallumon, 
+és a program törölje ennek a számnak valamennyi előfordulását a listából, majd írja ki a módosított listát a képernyőre!"""
+import random
+
+def DeleteNumb(numbers, delete):
+    return [x*x for x in numbers if x != delete]
+
+numbers = [] 
+for i in range(10): 
+    numbers.append(random.randint(1,3))
+
+print("Eredeti lista:", numbers)
+delete = int(input("Kérlek adj meg egy számot, amit törölni szeretnél a listából: "))
+
+print("Törlés után:", DeleteNumb(numbers, delete))
---
+++ 2025.10.15_lists/list_methods2.py
@@ -0,0 +1,25 @@
+"""1.2 Feladat
+Alakítsuk át az előbbi programot úgy, hogy a program arról adjon tájékoztatást, hogy pontosan hányszor szerepel a felhasználó által megadott szín a listában! Ha a megadott szín nincs még tárolva, továbbra is a "A megadott szín nem szerepel a listában." szöveg jelenjen meg!
+
+1.3 Feladat
+Egészítsük ki az előbbi programot úgy, hogy a kiértékelést követően a felhasználó által megadott szín kerüljön felvételre a listába, és csak ezután történjen meg a lista tartalmának kiírása!"""
+continue_playing = True
+colors = ["Kék", "Piros", "Zöld", "Lila", "Sárga", "Fekete", "Fehér", "Fehér", "Fehér", "Fekete"]
+while continue_playing:
+    
+    user_input = str(input("Kérlek adj meg 1 színt! (Nagy kezdőbetű és ékezetek!): "))
+
+    if user_input in colors:
+        print("A szined már szerepel a listában, méghozzá", colors.count(user_input), "x !")
+        x = input("Attól függetlenól szeretnéd hogy hozzáadjuk a listához? i/n")
+        if x == "i":
+            colors.append(user_input)
+            print("Akkor mostmár hozzá is van adva!")
+    else:
+        print("Még nem szerepel benne de most hozzá is adjuk rögvest!")
+        
+    continue_to_play = int(input("Kérlek add meg hogy szeretnéd e még folyatni! 1 ha igen, 2 ha nem! "))
+    if continue_to_play == 1:
+        print("Akkor jöjjön a következő kör!")
+    else:
+        continue_playing = False
